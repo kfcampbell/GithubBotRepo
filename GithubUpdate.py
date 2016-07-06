5ee1a856f86e491985d93282a9b764f2eed05069
kfcampbell <keeg4n.campbell@gmail.com>
2016-07-05 23:17:26 -0700

    Initial commit.

---
+++ GithubUpdate.py
@@ -0,0 +1,43 @@
+import os
+import time
+import base64
+from keys import token
+
+# get today's date
+time_now = (time.strftime("%m-%d-%Y-%I-%M-%S"))
+time_string = (time.strftime("%m/%d/%Y"))
+print time_now
+print time_string
+
+# variables to update the string
+name = "Keegan"
+email = "keeg4n.campbell@gmail.com"
+committer_string = "\"committer\": {\"name\": \"" + name + "\", \"email\": \"" + email + "\"}"
+# content = "Here's some basic content."
+# content_encoded = base64.b64encode(content)
+content = "bXkgbmV3IGZpbGUgY29udGVudHM="
+message = "Commit for " + time_string
+path = time_now + ".txt"
+branch = "master"
+link = "https://api.github.com/repos/kfcampbell/GithubBotRepo/contents/" + path
+
+# actual string to start with and add to
+command = "curl -i -X PUT -H \'Authorization: token "
+command += token
+# add the path
+command += "\' " + "-d \'{\"path\": \"" + path + "\", "
+# add the message
+command += "\"message\": \"" + message + "\", "
+# add the committer info
+command += committer_string + ", "
+# add the content
+command += "\"content\": \"" + content + "\", "
+# add the branch
+command += "\"branch\": \"" + branch + "\"}\' "
+# add the url
+command += link
+
+print command
+
+# now we run the thing.
+os.system(command)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Github Bot Project
+
+I know they got rid of streaks, but this is a bot I made to play with Github's API to post a new file every day.
+
+## Usage
+Create a new repo, then start a screen on your server and run a cron job to run the GithubUpdate.py script each day.
+
+## Things It Would Be Nice To Add
+
+[] a better way to construct a JSON request than just string manipulation
+[] simply update a single file rather than add a new file each time
+
