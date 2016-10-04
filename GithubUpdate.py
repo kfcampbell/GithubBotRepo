eb4e709319759ec2531af04dae87e6c843c89462
kfcampbell <keeg4n.campbell@gmail.com>
2016-10-03 17:55:44 -0700

    Added updates to single file.

--- GithubUpdate.py
+++ GithubUpdate.py
@@ -8,17 +8,21 @@ from keys import token, name, email
 # get today's date
 time_now = (time.strftime("%m-%d-%Y-%I-%M-%S"))
 time_string = (time.strftime("%m/%d/%Y"))
-print time_now
-print time_string
 
 # variables to update the string
+file_name = "daily_updates.txt"
 committer_string = "\"committer\": {\"name\": \"" + name + "\", \"email\": \"" + email + "\"}"
 content = "Today's date is " + time_string + "."
 content_encoded = base64.b64encode(content)
 message = "Commit for " + time_string
-path = "Updates/" + time_now + ".txt"
+# path = "Updates/" + time_now + ".txt"
+path = file_name
 branch = "master"
 link = "https://api.github.com/repos/kfcampbell/GithubBotRepo/contents/" + path
+
+# update the file with today's date.
+with open(file_name, "a") as daily_file:
+    daily_file.write(content + "\n")
 
 # actual string to start with and add to
 command = "curl -i -X PUT -H \'Authorization: token "
